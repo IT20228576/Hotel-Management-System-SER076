8682a8332ba3eb49005b4831b3f3300112f46f22
Chirath Jayawardena <88072810+IT20228576@users.noreply.github.com>
2022-08-25 20:19:55 +0530

    add basic server file

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+
+/* Loading the environment variables from the .env file. */
+dotenv.config();
+
+//
+// ─── SET UP SERVER ──────────────────────────────────────────────────────────────
+//
+
+/* Creating an instance of express. */
+const app = express();
+
+/* Setting the port to 8000. */
+const PORT = process.env.PORT || 8000;
+
+/* Starting the server on the port 8000. */
+app.listen(PORT, () =>
+  console.log(`Successfully Server started on : ${PORT}`)
+);
+
+/* A middleware that parses the body of the request and makes it available in the req.body property. */
+app.use(express.json());
+/* Parsing the cookie and making it available in the req.cookies property. */
+// app.use(cookieParser());
+/* Allowing the server to accept requests from the client. */
+app.use(
+  cors({
+    origin: ["http://localhost:1234"],
+    credentials: true,
+  })
+);
+
+//
+// ─── CONNECT TO MONGODB ─────────────────────────────────────────────────────────
+//
+
+mongoose.connect(
+  process.env.DBLINK,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Successfully Connected to MongoDB");
+  }
+);
+
+//
+// ─── SET UP ROUTES ──────────────────────────────────────────────────────────────
+//
+
